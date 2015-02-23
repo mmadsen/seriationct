33b350d7eb26e5cfd822249e8c47fd2906829eba
Mark Madsen <mark@madsenlab.org>
2015-02-23 15:12:23 -0800

    Trying to fix strange argparse error in Travis build.

--- simulations/sim-networkmodel-seriation.py
+++ simulations/sim-networkmodel-seriation.py
@@ -17,8 +17,8 @@ import math
 import argparse
 import pytransmission.popgen as pypopgen
 import simuOpt
-
-import seriationct as sct
+import uuid
+import ming
 import seriationct.data as data
 import seriationct.sampling as sampling
 import seriationct.utils as utils
@@ -27,7 +27,35 @@ simuOpt.setOptions(alleleType='long', optimized=True, quiet=False, numThreads=ut
 
 global config, sim_id, script
 
+def setup(parser):
+    config = parser.parse_args()
 
+    sim_id = uuid.uuid4().urn
+    script = __file__
+
+    if config.debug == '1':
+        log.basicConfig(level=log.DEBUG, format='%(asctime)s %(levelname)s: %(message)s')
+    else:
+        log.basicConfig(level=log.INFO, format='%(asctime)s %(levelname)s: %(message)s')
+
+    data.set_experiment_name(config.experiment)
+    data.set_database_hostname(config.dbhost)
+    data.set_database_port(config.dbport)
+    dbconfig = data.getMingConfiguration(data.modules)
+    ming.configure(**dbconfig)
+
+    # set up parallelism.  At the moment, this doesn't do anything on OSX
+    # but should provide parallelism on Linux across the replicates at least
+    cores = utils.get_parallel_cores(config.devel)
+    log.debug("Setting up %s cores for parallel simulation", cores)
+
+    import simuOpt
+    if(config.debug == 1):
+        simuOpt.setOptions(alleleType='long',optimized=True,quiet=False,numThreads = cores)
+    else:
+        simuOpt.setOptions(alleleType='long',optimized=True,quiet=True,numThreads = cores)
+
+    return (config,sim_id,script)
 
 def main():
     MAXALLELES = 10000000
@@ -52,7 +80,7 @@ def main():
     parser.add_argument("--popsize", help="Initial size of population for each community in the model", type=int, required=True)
     parser.add_argument("--migrationfraction", help="Fraction of population that migrates each time step", type=float, required=True, default=0.2)
 
-    (config, sim_id, script) = sct.setup(parser)
+    (config, sim_id, script) = setup(parser)
 
     log.info("config: %s", config)
 
