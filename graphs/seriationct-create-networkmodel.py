fed90c7a591c77f936dc11b28b91d171d77c11f6
Mark Madsen <mark@madsenlab.org>
2015-03-22 20:28:37 -0700

    Added --interlevel_weight parameter (default 1.0) for setting the weight of root/child and child/gchild edges.  This gives the flexibility to set 5 parameters and govern the weight matrix of the model.

--- graphs/seriationct-create-networkmodel.py
+++ graphs/seriationct-create-networkmodel.py
@@ -59,6 +59,7 @@ def setup():
     parser.add_argument("--gchild_interconnect_weight",help="the weight used for the gchild interconnect edges.", default=0.1)
     parser.add_argument("--gchild_interconnect_percentage", help="in the case of a hierarchical graph, what fraction of grandchildren are connected to each other (0-1.0) Default is 0.2", default=0.00)
     parser.add_argument("--child_interconnect_percentage", help="in the case of a hierarchical graph, what fraction of children are connected to each other (0-1.0) Default is 0.1", default=0.00)
+    parser.add_argument("--interlevel_weight", help="the weight used for edges between root/child or child/gchild nodes",type=float, default=1.0)
     parser.add_argument("--overlap",
                         help="specify % of nodes to overlap from slice to slice. 0=No overlap, 1 = 100% overlap. Values must be between 0.0 and 1.0. For example. 0.5 for 50%", default=0.80)
     parser.add_argument("--movie", help="make a movie from png slices.", default=True)
@@ -655,7 +656,7 @@ def wire_hierarchy(graph):
     ##  wire to root
     for node in list_of_children:
         key1=root+"*"+node
-        weight=1.0
+        weight=float(args.interlevel_weight)
         xcoord = nodeX[node]
         ycoord = nodeY[node]
         rootX = nodeX[root]
@@ -687,7 +688,7 @@ def wire_hierarchy(graph):
                 break
             list_of_grandchildren.append(gnode)
             key1=node+"*"+gnode
-            weight=1.0
+            weight=float(args.interlevel_weight)
             xcoord = nodeX[node]
             ycoord = nodeY[node]
             gnodeX = nodeX[gnode]
