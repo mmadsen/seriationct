4b7028573e61c9cc3f29893f6cf81b31f83a5a35
Mark Madsen <mark@madsenlab.org>
2015-03-28 10:31:29 -0700

    Annotation for non-hierarchical models by origin time, dark is old, light is recent

--- analytics/seriationct-annotate-minmax-graph.py
+++ analytics/seriationct-annotate-minmax-graph.py
@@ -14,6 +14,7 @@ import argparse
 import logging as log
 import zipfile
 import networkx as nx
+from networkx.utils import open_file, make_str
 import os
 from decimal import *
 from re import compile
@@ -26,6 +27,8 @@ def setup():
     parser.add_argument("--debug", type=int, help="turn on debugging output")
     parser.add_argument("--networkmodel", help="path to ZIP format network model containing GML slices", required=True)
     parser.add_argument("--inputfile", help="path to GML version of minmax seriation output file", required=True)
+    parser.add_argument("--modeltype", choices=['hierarchy', 'other'], required=True, default='other')
+    #parser.add_argument("--")
 
     args = parser.parse_args()
 
@@ -146,11 +149,267 @@ def copy_attributes_to_minmax(g_slice = None, g_mm = None):
             continue
 
         g_mm.node[mm_node_id]['appears_in_slice'] = g_slice.node[slice_node]['appears_in_slice']
-        g_mm.node[mm_node_id]['level'] = g_slice.node[slice_node]['level']
-        g_mm.node[mm_node_id]['child_of'] = g_slice.node[slice_node]['child_of']
-        g_mm.node[mm_node_id]['parent_node'] = g_slice.node[slice_node]['parent_node']
+        if args.modeltype == 'hierarchy':
+            g_mm.node[mm_node_id]['level'] = g_slice.node[slice_node]['level']
+            g_mm.node[mm_node_id]['child_of'] = g_slice.node[slice_node]['child_of']
+            g_mm.node[mm_node_id]['parent_node'] = g_slice.node[slice_node]['parent_node']
 
         log.debug("annotated: %s", g_mm.node[mm_node_id])
+
+
+def get_hierarchy_level_annotated_graphviz(input_graph, scheme):
+    """
+    Iterates over the nodes in a graph, looking for 'level' attributes.  For
+    each child, picks a color and caches it, and a color for the root, using the
+    specified graphviz color scheme.  Then, assigns
+    grandchildren the color of their 'child_of' attribute.  Adds the 'fillcolor' attribute
+    to each node in the NetworkX graph, and then returns the graph.
+    """
+
+        # make a copy so we don't touch the original graph, we'll return a new one
+    g = input_graph.copy()
+
+    # sometimes the "id" attribute is also the label, which we don't want for DOT production
+    g = nx.convert_node_labels_to_integers(g)
+
+    color_cache = dict()
+    color_list = range(1, 11) # rdylgn11 brewer scheme in graphviz
+    root_color = color_list.pop()
+
+    # figure out penwidth scaling
+    slice_ids = []
+    for node, data in g.nodes_iter(data=True):
+        slice_ids.append(g.node[node]['appears_in_slice'])
+
+    max_slice = max(slice_ids)
+    max_penwidth = 5.0
+
+    # grandchildren might point their child_of attribute at a node which is not present
+    # in this minmax graph given that we're operating with a sample of assemblages.
+    # So when we find a child_of that's not represented in the
+    for node, data in g.nodes_iter(data=True):
+
+        # check if we're getting low on new colors...
+        if len(color_list) == 0:
+            log.info("All colors in scheme have been used, recycling but keeping roots unique")
+            color_list = range(1,10)
+
+        lab = g.node[node]['label']
+        short_label = lab.replace('assemblage-','')
+        #del g.node[node]['label']
+        g.node[node]['short_label']= short_label
+
+        g.node[node]['colorscheme'] = scheme
+        g.node[node]['penwidth'] = (float(g.node[node]['appears_in_slice']) / max_slice) * max_penwidth
+
+        if g.node[node]['level'] == 'root':
+            g.node[node]['fillcolor'] = root_color
+            color_cache[node] = root_color
+            g.node[node]['shape'] = 'square'
+        elif g.node[node]['level'] == 'child':
+            name = g.node[node]['label']
+            g.node[node]['shape'] = 'diamond'
+            if name not in color_cache:
+                color = color_list.pop()
+                g.node[node]['fillcolor'] = color
+                color_cache[name] = color
+            else:
+                color = color_cache[name]
+                g.node[node]['fillcolor'] = color
+        elif g.node[node]['level'] == 'grandchild':
+            parent = g.node[node]['child_of']
+            g.node[node]['shape'] = 'circle'
+            if parent not in color_cache:
+                color = color_list.pop()
+                color_cache[parent] = color
+                g.node[node]['fillcolor'] = color
+            else:
+                color = color_cache[parent]
+                g.node[node]['fillcolor'] = color
+
+    for node, data in g.nodes_iter(data=True):
+        g.node[node]['label'] = g.node[node]['short_label']
+
+    return g
+
+
+def get_nonhierarchical__annotated_graphviz(input_graph):
+    """
+    Iterates over the nodes in a graph, annotating them for
+    origin time and duration.
+    """
+
+    # base scheme has color sets from 3 to 9
+    base_color_scheme = 'pubu'
+
+
+        # make a copy so we don't touch the original graph, we'll return a new one
+    g = input_graph.copy()
+
+    # sometimes the "id" attribute is also the label, which we don't want for DOT production
+    g = nx.convert_node_labels_to_integers(g)
+
+
+    # figure out penwidth scaling
+    slice_ids = set()
+    for node, data in g.nodes_iter(data=True):
+        slice_ids.add(g.node[node]['appears_in_slice'])
+
+    num_slices = len(slice_ids)
+
+    if num_slices > 9:
+        log.error("More slices than colors in the scheme list!!!")
+
+    if num_slices < 3:
+        log.info("Less slices than colors in the scheme, using pubu3")
+
+    scheme = base_color_scheme
+    scheme += str(len(slice_ids))
+
+    x = range(1,num_slices+1)
+    y = reversed(range(1,num_slices+1))
+
+    color_map = dict(zip(x,y))
+    log.debug("x: %s", x)
+    log.debug("y: %s", y)
+    log.debug("color_map: %s", color_map)
+
+
+    max_slice = max(slice_ids)
+    max_penwidth = 5.0
+
+    # grandchildren might point their child_of attribute at a node which is not present
+    # in this minmax graph given that we're operating with a sample of assemblages.
+    # So when we find a child_of that's not represented in the
+    for node, data in g.nodes_iter(data=True):
+
+
+        lab = g.node[node]['label']
+        short_label = lab.replace('assemblage-','')
+        #del g.node[node]['label']
+        g.node[node]['short_label']= short_label
+
+        g.node[node]['colorscheme'] = scheme
+        #g.node[node]['penwidth'] = (float(g.node[node]['appears_in_slice']) / max_slice) * max_penwidth
+
+        g.node[node]['fillcolor'] = color_map[int(g.node[node]['appears_in_slice'])]
+
+    for node, data in g.nodes_iter(data=True):
+        g.node[node]['label'] = g.node[node]['short_label']
+
+    return g
+
+
+
+
+
+def write_ordered_dot(N,path,name=None):
+    """Write NetworkX graph G to Graphviz dot format on path.
+
+    Path can be a string or a file handle.
+    """
+    try:
+        import pydot
+    except ImportError:
+        raise ImportError("write_dot() requires pydot",
+                          "http://code.google.com/p/pydot/")
+    P=generate_ordered_dot(N, name)
+
+    title_string = "\nlabelloc=\'t\';label=\'%s\';}\n" % name
+
+    p = P.to_string();
+    # p2 = re.sub("\n\}\n", "", p)
+    # p2 += "\nlabelloc=\"t\";\n"
+    # p2 += "label=\"%s\";\n}\n" % name
+
+    with open(path, 'wb') as pathfile:
+        pathfile.write(p)
+    return
+
+
+
+
+def generate_ordered_dot(N, name=None):
+    """
+    The networkx write_dot() function generates
+    """
+    try:
+        import pydot
+    except ImportError:
+        raise ImportError('to_pydot() requires pydot: '
+                          'http://code.google.com/p/pydot/')
+
+    # set Graphviz graph type
+    if N.is_directed():
+        graph_type='digraph'
+    else:
+        graph_type='graph'
+    strict=N.number_of_selfloops()==0 and not N.is_multigraph()
+
+    node_attrs = dict()
+    node_attrs["shape"] = "circle"
+    node_attrs["width"] = "0.3"
+    node_attrs["height"] = "0.3"
+    node_attrs["label"] = ""
+    #node_attrs["fixedsize"] = "true"
+    node_attrs["style"] = "filled"
+
+    graph_defaults=N.graph.get('graph',{})
+    graph_defaults["ratio"] = "auto"
+    graph_defaults["labelloc"] = "b"
+    graph_defaults["label"] = name
+    graph_defaults["pad"] = "1.0"
+
+
+    if name is None:
+        P = pydot.Dot(graph_type=graph_type,strict=strict,**graph_defaults)
+    else:
+        P = pydot.Dot('"%s"'%name,graph_type=graph_type,strict=strict,
+                      **graph_defaults)
+    try:
+        P.set_node_defaults(**node_attrs)
+    except KeyError:
+        pass
+    try:
+        P.set_edge_defaults(**N.graph['edge'])
+    except KeyError:
+        pass
+
+    for n,nodedata in sorted(N.nodes_iter(data=True), key=lambda n: int(n[0])):
+        str_nodedata=dict((k,make_str(v)) for k,v in nodedata.items())
+
+        if "name" in str_nodedata:
+            del str_nodedata['name']
+
+        #print "str_nodedata: %s" % str_nodedata
+        p=pydot.Node(make_str(n),**str_nodedata)
+        P.add_node(p)
+
+    if N.is_multigraph():
+        for u,v,key,edgedata in N.edges_iter(data=True,keys=True):
+            str_edgedata=dict((k,make_str(v)) for k,v in edgedata.items())
+            edge=pydot.Edge(make_str(u),make_str(v),key=make_str(key),**str_edgedata)
+            P.add_edge(edge)
+
+    else:
+
+
+
+        for u,v,edgedata in sorted(N.edges_iter(data=True), key=lambda u: int(u[0]) ):
+            str_edgedata=dict((k,make_str(v)) for k,v in edgedata.items())
+            if int(v) < int(u):
+                edge = pydot.Edge(make_str(v),make_str(u),**str_edgedata)
+            else:
+                edge=pydot.Edge(make_str(u),make_str(v),**str_edgedata)
+            P.add_edge(edge)
+    return P
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -160,6 +419,8 @@ if __name__ == "__main__":
     input_basename = os.path.basename(args.inputfile)
     root, ext = os.path.splitext(input_basename)
     input_path = os.path.dirname(args.inputfile)
+    if input_path is '':
+        input_path = '.'
     output_filename = input_path + '/' + root + "-annotated.gml"
     log.info("Processing input %s to output %s", input_basename, output_filename)
 
@@ -182,6 +443,26 @@ if __name__ == "__main__":
     nx.write_gml(mm, output_filename)
 
 
+    if args.modeltype == 'hierarchy':
+        # for hierarchical models, label them with their level and child groupings and
+        # write a graph to the filesystem
+        gv_annotated = get_hierarchy_level_annotated_graphviz(mm, 'rdylgn11')
+        dot_filename = input_path + '/' + root + "-annotated-hierarchy.dot"
+        png_filename = input_path + '/' + root + "-annotated-hierarchy.png"
+    else:
+        # for other models, annotate the graph with origin time and duration of assemblages
+        gv_annotated = get_nonhierarchical__annotated_graphviz(mm)
+        dot_filename = input_path + '/' + root + "-annotated-chronological.dot"
+        png_filename = input_path + '/' + root + "-annotated-chronological.png"
+
+    write_ordered_dot(gv_annotated, dot_filename)
+
+    cmd = "neato -Tpng "
+    cmd += dot_filename
+    cmd += " -o "
+    cmd += png_filename
+
+    os.system(cmd)
 
 
 
