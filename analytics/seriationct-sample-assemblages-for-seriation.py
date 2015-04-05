7dfbd64288d6c93de249ca9e3eeeb8b6e97afac4
Mark Madsen <mark@madsenlab.org>
2015-04-05 10:39:47 -0700

    Refactoring assemblage sampling script to provide --sampletype and --numsamples options, to take different types of samples, and generate N independent output samples from each input file. Sample type "random" is working now, generating a uniform random sample of assemblages.

--- analytics/seriationct-sample-assemblages-for-seriation.py
+++ analytics/seriationct-sample-assemblages-for-seriation.py
@@ -24,7 +24,11 @@ def setup():
     parser.add_argument("--debug", type=int, help="turn on debugging output")
     parser.add_argument("--inputdirectory", help="path to directory with IDSS format input files to sample", required=True)
     parser.add_argument("--outputdirectory", help="path to directory for sampled data files", required=True)
-    parser.add_argument("--samplefraction", type=float, help="Sample size to resample frequencies for each sim run and replication")
+    parser.add_argument("--samplefraction", type=float, help="Sample size to resample frequencies for each sim run and replication", required=True)
+    parser.add_argument("--sampletype", choices=['random', 'spatial', 'temporal', 'spatiotemporal'],
+                        help="type of sampling.  random has no stratification, temporal is rough early/late stratification, spatial is"
+                            "quadrants, spatiotemporal is stratification by both", required=True)
+    parser.add_argument("--numsamples", type=int, help="number of samples to take from each original data set (default is 1)", default=1)
 
 
     args = parser.parse_args()
@@ -62,45 +66,57 @@ def read_unsampled_file(filename):
 
 
 
+def random_sample_without_stratification(row_list):
+    """
+    Takes a uniform random sample of rows without any stratification
+    Returns a list of row indices that can be used to select the
+    sample points from the original rows in the file
+    """
+    num_rows = len(row_list)
 
+    num_samples = int(math.ceil(args.samplefraction * num_rows))
+    log.info("Taking %s samples from file %s", num_samples, file)
 
+    sampled_rows = random.sample(range(0, num_rows), num_samples)
+    log.debug("Sampling rows: %s", sampled_rows)
+
+    return sampled_rows
 
 
 if __name__ == "__main__":
     setup()
 
     for file in os.listdir(args.inputdirectory):
+        log.info("Starting processing of %s", file)
         if fnmatch.fnmatch(file, '*.txt'):
             root = parse_filename_into_root(file)
 
-            outputfile = args.outputdirectory + "/" + root + "-sampled-" + str(args.samplefraction) + ".txt"
-
-            log.info("Starting processing of %s", outputfile)
-
             (header, row_list) = read_unsampled_file(file)
-
             log.debug("header: %s", header)
 
+            # create N independent samplings from each input file
+            for sample_num in range(0, args.numsamples):
+                outputfile = args.outputdirectory + "/" + root + "-sampled-" + str(args.samplefraction) + "-sample-" + str(sample_num) + ".txt"
 
-            num_rows = len(row_list)
+                if args.sampletype == 'random':
+                    sampled_rows = random_sample_without_stratification(row_list)
+                elif args.sampletype == 'spatial':
+                    pass
+                elif args.sampletype == 'temporal':
+                    pass
+                elif args.sampletype == 'spatiotemporal':
+                    pass
 
-            num_samples = int(math.ceil(args.samplefraction * num_rows))
-            log.info("Taking %s samples from file %s", num_samples, file)
+                log.info("Writing sampled output for file: %s ", outputfile)
 
-            sampled_rows = random.sample(range(0, num_rows), num_samples)
-            log.debug("Sampling rows: %s", sampled_rows)
+                with open(outputfile, 'wb') as outfile:
+                    outfile.write(header)
 
+                    for row_idx in sampled_rows:
+                        row = row_list[row_idx]
+                        row_str = '\t'.join(row)
+                        row_str += '\n'
+                        outfile.write(row_str)
 
-            log.info("Writing sampled output for file: %s ", outputfile)
-
-            with open(outputfile, 'wb') as outfile:
-                outfile.write(header)
-
-                for row_idx in sampled_rows:
-                    row = row_list[row_idx]
-                    row_str = '\t'.join(row)
-                    row_str += '\n'
-                    outfile.write(row_str)
-
-            log.info("Completed processing of file %s", outputfile)
+        log.info("Completed processing of file %s", outputfile)
 
--- testdata/test6x6-0.25-griddistance/test6x6-0.25-griddistance-5.gml
+++
@@ -1,9 +0,0 @@
-graph [
-  is_directed 0
-  node [
-    id 0
-    label "assemblage-3-1"
-    xcoord 3
-    ycoord 1
-  ]
-]
