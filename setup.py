f81857cf8a37e7db4564c8dd572fcf319854a17a
Mark Madsen <mark@madsenlab.org>
2015-04-09 14:55:49 -0700

    Bump version number to 1.1 for tagging

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ Description here
 from distutils.core import setup
 
 setup(name="seriationct",
-      version="1.0",
+      version="1.1",
       packages = [
         'seriationct',
         'seriationct.data',
