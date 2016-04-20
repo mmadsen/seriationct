4c34b89eb6346baf7b4717684fe9534319431f48
Mark Madsen <mark@madsenlab.org>
2016-04-20 12:05:59 -0700

    Finished modifying experiment template for simulation post processing, with and without gridengine.

--- experiment-template/bin/simulation-postprocess-gridengine.sh
+++ experiment-template/bin/simulation-postprocess-gridengine.sh
@@ -19,8 +19,22 @@ seriationct-resample-builder.py --inputdirectory exported-data \
     --parallelism 1
 
 
-for d in `ls jobs/resamplejob*.sh`; do ( sh $d ); done
+for d in `ls jobs/resamplejob*.sh`
+do
+        qsub -V -cwd $d
+done
 
+# just a default value, but this is often what we run at a time given core count
+count=10
+
+while [ $count -ne 0 ]
+do
+	sleep 60
+	count=`qstat | wc -l`
+	echo "still $count exports running in gridengine"
+done
+
+echo "...resampling assemblages complete..."
 
 
 ######## Sample assemblages to pull one assemblage per time interval, with no overlap ########
@@ -39,11 +53,27 @@ seriationct-simulation-sample-assemblages-builder.py --inputdirectory sampled-tr
     --parallelism 1
 
 
-for d in `ls jobs/assemsamplejob*.sh`; do ( sh $d ); done
+for d in `ls jobs/assemsamplejob*.sh`
+do
+        qsub -V -cwd $d
+done
+
+# just a default value, but this is often what we run at a time given core count
+count=10
+
+while [ $count -ne 0 ]
+do
+	sleep 60
+	count=`qstat | wc -l`
+	echo "still $count exports running in gridengine"
+done
+
+echo "...sampling sassemblages complete..."
 
 ######## Filter slice-stratified assemblages to eliminate types with less than 3 non-zero entries #######
 
 echo "==================== filter subsampled assemblages ====================="
+
 
 
 seriationct-simulation-filter-types-builder.py --inputdirectory assemblage-sampled \
@@ -56,7 +86,22 @@ seriationct-simulation-filter-types-builder.py --inputdirectory assemblage-sampl
     --jobdirectory jobs \
     --parallelism 1
 
-for d in `ls jobs/filterjob*.sh`; do ( sh $d ); done
+for d in `ls jobs/filterjob*.sh`
+do
+        qsub -V -cwd $d
+done
+
+# just a default value, but this is often what we run at a time given core count
+count=10
+
+while [ $count -ne 0 ]
+do
+	sleep 60
+	count=`qstat | wc -l`
+	echo "still $count exports running in gridengine"
+done
+
+echo "...filtering assemblages complete..."
 
 
 ######### Prepare seriation input with all the info needed for spatial seriation and later annotation
@@ -67,7 +112,6 @@ seriationct-finalize-seriation-input.py \
     --experiment REPLACEME \
     --inputdirectory filtered-data \
     --debug 0
-
 
 
 
